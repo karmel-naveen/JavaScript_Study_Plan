97ffc5adfc325bd3601da69968bedab23f02d83e
Karmel Naveen M S <naveenkarmel@gmail.com>
2022-06-10 11:21:37 +0530

    Day one completed

---
+++ 30-Days-Of-JavaScript/01_Day_Introduction/datatypes.js
@@ -0,0 +1,6 @@
+let var1 = 'Hello World';
+let var2 = 100;
+let var3 = true;
+let var4 = null;
+let var5 = undefined;
+console.log(typeof var1,typeof var2,typeof var3,typeof var4,typeof var5);
---
+++ 30-Days-Of-JavaScript/01_Day_Introduction/exercise1.js
@@ -0,0 +1,13 @@
+// comments can make code readable
+
+// Welcome to 30DaysOfJavaScript
+
+/* comments can make code readable, easy to reuse and informative */
+
+// Declaring different variables of different data types on same line
+let firstName = 'Karmel',lastName = 'Naveen',country= 'India',city = 'Coimbatore',age = 22,isMarried =false
+
+let x = 45
+let y = 30
+console.log('I am '+x+' years old.')
+console.log('You are '+y+' years old.')
